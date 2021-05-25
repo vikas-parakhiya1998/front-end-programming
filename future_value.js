fe507f9b85b89bc2a4e3ee991d52164cffd0d141
vikas-parakhiya1998 <81839272+vikas-parakhiya1998@users.noreply.github.com>
2021-05-25 11:41:20 -0400

    Add files via upload

---
+++ future_value.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// get investment amount - loop until user enters a number
+let investment = 0;
+do {
+    investment = parseFloat(
+        prompt("Enter investment amount as xxxxx.xx", 10000));
+}
+while ( isNaN(investment) );
+
+// get interest rate - loop until user enters a number
+let rate = 0;
+do {
+    rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
+}
+while ( isNaN(rate) );
+
+// get number of years - loop until user enters a number
+let years = 0;
+do {
+    years = parseInt(prompt("Enter number of years", 10));
+}
+while ( isNaN(years) );
+
+// calulate future value
+let futureValue = investment;
+for (let i = 1; i <= years; i++ ) {
+    futureValue += futureValue * rate / 100;
+}
+
+
+// calulate future value montly
+const months = years*12;
+let futureValuemonthly = investment;
+for (let i = 1; i <= months; i++ ) {
+    futureValuemonthly = futureValuemonthly + (futureValuemonthly * rate / 12/100);
+}
+
+
+// display results
+document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
+document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
+document.write(`<p><label>Years:</label> ${years}</p>`);
+document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
+
+// display results
+document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
+document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
+document.write(`<p><label>Years:</label> ${years}</p>`);
+document.write(`<p><label>Future Value monthly:</label> ${futureValuemonthly.toFixed(2)}</p>`);
